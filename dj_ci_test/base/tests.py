6ab798abb81b970ed45eb9d876b505bea2356da8
smant <smirnoff.anton@gmail.com>
2013-12-19 17:30:31 +0300

    Update tests.py

--- dj_ci_test/base/tests.py
+++ dj_ci_test/base/tests.py
@@ -11,7 +11,7 @@ from django.test import TestCase
 
 class SimpleTest(TestCase):
 
-    @expectedFailure
+    #@expectedFailure
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
